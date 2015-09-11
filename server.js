e2f631a52fae42704068c41f82d8b8126ed8aad9
Philip London <philip.london@borngroup.com>
2015-09-11 11:12:25 -0400

    remove dep to public folder

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
